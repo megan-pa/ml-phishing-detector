9c69dd243ae5282c7fccbc886b98bd7bde0f57db
Megan Parfitt <za23370@bristol.ac.uk>
2025-12-28 14:04:49 +0000

    logisitic regression pipeline created

--- model.py
+++ model.py
@@ -1,6 +1,10 @@
 import re
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 
 df = pd.read_csv('archive/phishing_email.csv')
 df = df.dropna(subset=["text_combined", "label"])
@@ -17,3 +21,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 
 X_train = X_train.apply(clean_text)
 X_test = X_test.apply(clean_text)
+
+# vectorisises data and fits to logistic regression model
+pipe = Pipeline(
+    [
+        ('vectorizer', TfidfVectorizer()),
+        ('classifier', LogisticRegression(random_state=42, max_iter=1000))
+    ]
+)
+
+pipe.fit(X_train, y_train)
+print("Accuracy: ", pipe.score(X_test, y_test))
+y_pred = pipe.predict(X_test)
+print(classification_report(y_test, y_pred))
