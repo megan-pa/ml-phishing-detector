59ab8b40b3177dcab424d1e99a9b5a28c54fb4b5
Megan Parfitt <za23370@bristol.ac.uk>
2025-12-28 23:33:11 +0000

    adding svm model and accuracy function

--- model.py
+++ model.py
@@ -4,7 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn import svm
 
 df = pd.read_csv('archive/phishing_email.csv')
 df = df.dropna(subset=["text_combined", "label"])
@@ -22,15 +23,32 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 X_train = X_train.apply(clean_text)
 X_test = X_test.apply(clean_text)
 
-# vectorisises data and fits to logistic regression model
+# logisitic regression model pipeline
 pipe = Pipeline(
     [
         ('vectorizer', TfidfVectorizer()),
-        ('classifier', LogisticRegression(random_state=42, max_iter=1000))
+        ('classifier', LogisticRegression(random_state=42, max_iter=1000, class_weight='balanced'))
     ]
 )
 
-pipe.fit(X_train, y_train)
-print("Accuracy: ", pipe.score(X_test, y_test))
-y_pred = pipe.predict(X_test)
-print(classification_report(y_test, y_pred))
+# linear svm model pipeline
+svm_pipe = Pipeline(
+    [
+        ('vectorizer', TfidfVectorizer()),
+        ('classifier', svm.SVC(kernel='linear', C=1, random_state=42))
+    ]
+)
+
+# function to evaluate accuracy of models
+def model_accuracy(model, X_train, y_train, X_test, y_test, name="Model"):
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    print(f"{name} Accuracy: {accuracy:.4f}")
+    print(classification_report(y_test, y_pred))
+
+    return accuracy
+
+model_accuracy(pipe, X_train, y_train, X_test, y_test, name="Logistic Regression")
+model_accuracy(svm_pipe, X_train, y_train, X_test, y_test, name="Linear SVM")
