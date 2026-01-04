af36fbdfc40fb56f6ee4317c35ef07ac21593dcd
Megan Parfitt <za23370@bristol.ac.uk>
2026-01-04 20:14:56 +0000

    edits to api code

--- api/main.py
+++ api/main.py
@@ -1,32 +1,37 @@
 import joblib
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
 from typing import List
 from detector import final_decision
-
-class EmailBatch(BaseModel):
-    email_texts: List[str]
-
-app = FastAPI()
+from contextlib import asynccontextmanager
 
 BEST_MODEL = "artifacts/best_phishing_model.pkl"
-MODEL = joblib.load(BEST_MODEL)
 
-@app.get("/")
-def root():
-    return {"message": "Detector API running"}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.model = joblib.load(BEST_MODEL)
+    yield
 
-# TODO allow users to input emails as files as well as string
+app = FastAPI(lifespan=lifespan)
+
 @app.post("/classify_email")
-def classify_email(email_text: str):
-    detector_result = final_decision(email_text=email_text, model=MODEL)
+def quick_classify_email(email_text: str):
+    detector_result = final_decision(email_text=email_text, model=app.state.model)
+    return {"classification": detector_result}
+
+@app.post("/classify_email_file")
+async def classify_email(email_file: UploadFile = File(...)):
+    contents = await email_file.read()
+    detector_result = final_decision(email_text=contents.decode('utf-8'), model=app.state.model)
     return {"classification": detector_result}
 
 @app.post("/classify_email_batch")
-def classify_email_batch(email_texts: EmailBatch):
+async def classify_email_batch(email_files: List[UploadFile] = File(...)):
     results = []
-    for email in email_texts.email_texts:
-        detector_result = final_decision(email_text=email, model=MODEL)
+
+    for email in email_files:
+        contents = await email.read()
+        detector_result = final_decision(email_text=contents.decode('utf-8'), model=app.state.model)
         results.append(detector_result)
     return {"batch_classification": results}
 
